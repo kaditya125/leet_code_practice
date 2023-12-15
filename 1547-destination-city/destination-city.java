e206024b6045080bd5a8786135a19e34934d341e
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2023-12-15 14:41:30 +0530

    Time: 2 ms (79.52%) | Memory: 43.3 MB (23.46%) - LeetSync

---
+++ 1547-destination-city/destination-city.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashMap<String, Integer> outdegree = new HashMap<String, Integer>();
+        for(List<String> x: paths)
+            if(outdegree.containsKey(x.get(0)))
+                outdegree.put(x.get(0), outdegree.get(x.get(1))+1);
+            else
+               outdegree.put(x.get(0), 1);
+        
+        for(List<String> x: paths)
+               if(!outdegree.containsKey(x.get(0)))
+                    return x.get(0);
+               else if(!outdegree.containsKey(x.get(1)))
+                    return x.get(1);
+            
+        return paths.get(0).get(0);
+
+    }
+}
