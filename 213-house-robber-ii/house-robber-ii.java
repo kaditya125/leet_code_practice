bd0131321c5d9bc1ae444cde8316c72e5565a8f5
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2023-12-17 01:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (19.26%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,32 +1,40 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        
+
         if (n == 0) {
             return 0;
         } else if (n == 1) {
             return nums[0];
         }
 
-        // Rob the first house and exclude the last house
+        // Case 1: Rob the first house and exclude the last house
         int max1 = robHelper(nums, 0, n - 2);
-        
-        // Do not rob the first house and include the last house
+
+        // Case 2: Do not rob the first house and include the last house
         int max2 = robHelper(nums, 1, n - 1);
-        
+
+        // Return the maximum of the two cases
         return Math.max(max1, max2);
     }
-    
-    private int robHelper(int[] nums, int start, int end) {
-        int prevMax = 0;
-        int currMax = 0;
 
-        for (int i = start; i <= end; i++) {
-            int temp = currMax;
-            currMax = Math.max(currMax, prevMax + nums[i]);
-            prevMax = temp;
+    private int robHelper(int[] nums, int start, int end) {
+        int n = end - start + 1;
+
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[start];
         }
 
-        return currMax;
+        int[] dp = new int[n];
+        dp[0] = nums[start];
+        dp[1] = Math.max(nums[start], nums[start + 1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[start + i]);
+        }
+
+        return dp[n - 1];
     }
 }
