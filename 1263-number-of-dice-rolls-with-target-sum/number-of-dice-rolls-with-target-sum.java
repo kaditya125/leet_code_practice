563f3e00f528ae44de956bcb7aa688ec4c57f7fe
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2023-12-26 18:16:02 +0530

    Time: 9 ms (86.30%) | Memory: 42.3 MB (22.06%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,32 +1,39 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    private Map<String, Integer> memo;
-
+    Integer[][] dp;
+    int mod= 1000000007;
     public int numRollsToTarget(int n, int k, int target) {
-        memo = new HashMap<>();
-        return helper(n, k, target);
+        
+        dp = new Integer[n+1][target+1];
+        return targetres(n,k,target);
+        
     }
 
-    private int helper(int n, int k, int target) {
-        if (n == 0 || target <= 0) {
-            return (n == 0 && target == 0) ? 1 : 0;
-        }
-
-        String key = n + "-" + target;
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        }
-
-        int res = 0;
-        for (int i = 1; i <= k; i++) {
-            if (target - i >= 0) {
-                res = (res + helper(n - 1, k, target - i)) % 1000000007;
+    public int targetres(int n ,int k, int target)
+    {
+        if(n==0)
+        {
+            if(target==0)
+            {
+                return 1;
+            }else{
+                return 0;
             }
         }
+        if(target<0)
+        {
+            return 0;
+        }
+        if(dp[n][target]!=null)
+        {
+            return dp[n][target];
+        }
+        int res=0;
+        for(int i=1;i<=k;i++)
+        {
+           res+=targetres(n-1,k,target-i);
+           res %=mod;
+        }
 
-        memo.put(key, res);
-        return res;
+        return dp[n][target]=res;
     }
 }
