61e3871284af75dbafd5a34888975db042122136
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-01-03 19:25:22 +0530

    Time: 48 ms (56.18%) | Memory: 73.5 MB (90.16%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -4,23 +4,23 @@ class Solution {
             return 0;
         }
 
-        
-        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+        // Sort intervals based on their end points
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
 
         int nonOverlappingCount = 1;
         int lastEnd = intervals[0][1];
 
         for (int i = 1; i < intervals.length; i++) {
-
+            // Check for overlap
             if (intervals[i][0] >= lastEnd) {
-                
+                // Non-overlapping interval found, update lastEnd
                 nonOverlappingCount++;
                 lastEnd = intervals[i][1];
             }
-            
+            // If there is an overlap, skip this interval
         }
 
-       
+        // Calculate the number of intervals to be removed
         int removals = intervals.length - nonOverlappingCount;
         return removals;
     }
