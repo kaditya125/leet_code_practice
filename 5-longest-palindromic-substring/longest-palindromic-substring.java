27579e5275a87f76c1d336e6b9e60620fd7835df
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-01-04 23:19:04 +0530

    Time: 124 ms (40.52%) | Memory: 55.6 MB (5.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,33 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int start = 0;
+        int[][] dp = new int[n][n];
         int maxLength = 1;
+        int start = 0;
 
         // All substrings of length 1 are palindromes
         for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
+            dp[i][i] = 1;
         }
 
         // Check substrings of length 2
         for (int i = 0; i < n - 1; i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                start = i;
+                dp[i][i + 1] = 2;
                 maxLength = 2;
+                start = i;
             }
         }
 
         // Check substrings of length 3 and above
         for (int len = 3; len <= n; len++) {
             for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;  // Ending index of the substring
+                int j = i + len - 1;
 
-                if (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = true;
-                    start = i;
-                    maxLength = len;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1] > 0) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+
+                    if (dp[i][j] > maxLength) {
+                        maxLength = dp[i][j];
+                        start = i;
+                    }
                 }
             }
         }
