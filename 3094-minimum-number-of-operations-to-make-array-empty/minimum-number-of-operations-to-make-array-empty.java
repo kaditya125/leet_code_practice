ee1030a6e56f144b9ddfb2b1883da5d9bad78a35
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-01-04 12:33:46 +0530

    Time: 18 ms (85.66%) | Memory: 60.5 MB (23.40%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.java
@@ -0,0 +1,23 @@
+class Solution {
+  public int minOperations(int[] nums) {
+    int ans = 0;
+    Map<Integer, Integer> count = new HashMap<>();
+
+    for ( int num : nums)
+    {
+        count.put(num,count.getOrDefault(num,0)+1);
+    }
+     // count.merge(num, 1, Integer::sum);
+
+    for (final int freq : count.values()) {
+      // If freq == 3k, need k operations.
+      // If freq == 3k + 1 = 3*(k - 1) + 2*2, need k + 1 operations.
+      // If freq == 3k + 2, need k + 1 operations.
+      if (freq == 1)
+        return -1;
+      ans += (freq + 2) / 3;
+    }
+
+    return ans;
+  }
+}
