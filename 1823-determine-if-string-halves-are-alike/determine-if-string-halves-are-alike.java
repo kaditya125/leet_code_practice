551c3d4cd7896969c7758126d2ed9d0671c8b7c9
ADITYA KUMAR <80103410+kaditya125@users.noreply.github.com>
2024-01-12 13:50:50 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (24.13%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,34 +1,25 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-       int n=s.length();
-       int half=n/2;
-       String str1= s.substring(0,half);
-       String str2= s.substring(half,n);
-    int count1=0;
-    int count2=0;
-        for(int i=0;i<str1.length();i++)
-        {
-            char ch=str1.charAt(i);
-            if(ch=='a' || ch=='e' || ch== 'i' || ch== 'o' || ch=='u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-            {
-                count1++;
+        int n = s.length();
+        int half = n / 2;
+        String str1 = s.substring(0, half);
+        String str2 = s.substring(half, n);
+        
+        int count1 = countVowels(str1);
+        int count2 = countVowels(str2);
+
+        return count1 == count2;
+    }
+
+    private int countVowels(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                    || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                count++;
             }
-
         }
-         for(int i=0;i<str2.length();i++)
-        {
-            char ch=str2.charAt(i);
-            if(ch=='a' || ch=='e' || ch== 'i' || ch== 'o' || ch=='u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-            {
-                count2++;
-            }
-
-        }
-
-        if(count1==count2)
-        {
-            return true;
-        }
-        return false;
+        return count;
     }
 }
